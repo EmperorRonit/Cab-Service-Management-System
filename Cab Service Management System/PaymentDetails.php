d0c09fafd3cd5056c804fcd2465b5b3fb801e31c
RonitK <ronitkohinkar001@gmail.com>
2022-03-22 16:04:18 +0530

    All Css Complete

--- Cab Service Management System/PaymentDetails.php
+++ Cab Service Management System/PaymentDetails.php
@@ -1,26 +1,25 @@
-<?php
-include("config.php");
-
+<?php 
 if(isset($_GET['username']) && isset($_GET['logcount'])){
     $logcount=$_GET['logcount'];
     $username=$_GET['username'];
-}
-
-$res = mysqli_query($mysqli, "select * from packagetb");
-
+  }
 ?>
+
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Package</title>
-        <link rel="stylesheet" href="csms.css">
-    <meta charset="utf-8">
+<head>
+<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="CssFile.css">
-    </head>
-    <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <link rel="stylesheet" href="Aboutus.css">
+    <link rel="stylesheet" href="PaymentDetails.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <i class="material-icons" style="font-size:48px;color:white;text-shadow:2px 2px 4px #000000;">local_taxi</i>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,7 +34,7 @@ $res = mysqli_query($mysqli, "select * from packagetb");
                         <a class="nav-link active" aria-current="page" href="customer_details.php?logcount=<?php echo $logcount?>&username=<?php echo $username?>">Details</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="fleets.php?logcount=<?php echo $logcount?>&username=<?php echo $username?>">Fleets</a>
+                        <a class="nav-link active" aria-current="page" href="fleets.php?logcount=<?php echo $logcount?>&username=<?php echo $username?>">Fleets</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="package_customer.php?logcount=<?php echo $logcount?>&username=<?php echo $username?>" name="pakcage">Packages</a>
@@ -46,11 +45,12 @@ $res = mysqli_query($mysqli, "select * from packagetb");
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="feedback_customer.php?logcount=<?php echo $logcount?>&username=<?php echo $username?>">Feedback</a>
                     </li>
-                </ul>
+                </ul> 
                 <?php
                 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true || $logcount==1){
                     echo "<a href='index.php'><button class='btn btn-outline-light' type='submit' name='logout'>Log Out</button></a>";
-                  }
+                    session_destroy();
+                }
                 else{
                     echo "<a href='signin.php?logcount=".$logcount."&username=".$username."'><button class='btn btn-outline-light' type='submit'>Sign In</button></a>";
                     echo "<a href='signup.php?logcount=".$logcount."&username=".$username."'><button class='btn btn-outline-light' type='submit'>Sign Up</button></a>";
@@ -58,35 +58,23 @@ $res = mysqli_query($mysqli, "select * from packagetb");
                 ?>
             </div>
         </div>
-    </nav>
-    <div class="container">
-        <h2 style="margin-top: 20px;">Packages</h2>
-        <br><br>
-    <table class="table table-striped table-dark">
-        <thead>
-    <tr>
-      <th scope="col">Package Description</th>
-      <th scope="col">Cab Type</th>
-      <th scope="col">Capacity</th>
-      <th scope="col">Price</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-    <?php
-    while($row = mysqli_fetch_array($res)){
-        echo "<tbody>";
-        echo "<tr>";
-        echo "<td>".$row['package_dec']."</td>";
-        echo "<td>".$row['cab_type']."</td>";
-        echo "<td>".$row['capacity']."</td>";
-        echo "<td>".$row['package_price']."</td>";
-        echo "<td><a href='package_booking_customer.php?logcount=".$logcount."&package_id=".$row['package_id']."&username=".$username."'><button class='btn btn-outline-light btn-sm' type='submit'>Book</button></a></td>";
-        echo "</tr>";
-        echo"</tbody>";
-    }
-    ?>
-    </table>     
+    </nav>-->
+<div>
+<img src="Images/paymentgate.jpg" alt="Paris" style="height: 520px;  width: 640px; margin-top: 30px; margin-bottom: -520px;"> 
+    <Fieldset>
+    <legend><h1><u>Bank Details:</u></h1></legend><br>
+    <br>
+    <h2>Bank Name: &nbsp&nbsp&nbsp&nbsp&nbsp <strong><u>State Bank of India,</u></strong></h2>
+
+    <h2>State Bank Of India(IFSC Code):&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<strong><u>SBIN0000300</u></strong></h2>
+
+    <h2>State Bank Of India(Account No):&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<strong><u>2435117781</u></strong></h2>
+
+    <h2>State Bank Of India(Branch):&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<strong><u>ShivajiNagar Pune,Maharashtra-1800 11 2211</u></strong></h2>
+    
+    <h2>UPI Payment:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<strong><u>8790980979</u></strong></h2>
+         
+</Fieldset>
 </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
+</body>
 </html>
